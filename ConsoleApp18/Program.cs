e4a2de18d31b1b261553b1e934b5c8a0955e53c6
Zeynalbey <zeynaleg@code.edu.az>
2022-06-05 21:13:28 +0400

    recent changes for currency-task

--- ConsoleApp18/Program.cs
+++ ConsoleApp18/Program.cs
@@ -6,94 +6,74 @@ namespace ConsoleApp18
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("1./show-recent-currency-rates");
-            Console.WriteLine("2./find-currency-rate-by-code");
-            Console.WriteLine("3./calculate-amount-by-currecy");
-            Console.WriteLine("4./exit");
-
-
-
-
-
-            //USD, RUB, TRY
-
-            string[] array = { "USD", "RUB", "TRY","a" };
-            double[] array1 = { 1.70, 2,3,7 };
-
-            double amount = Convert.ToDouble(Console.ReadLine());
-            bool notFound = true;
-
-            if (amount == 1)
+            while (true)
             {
-                for (int i = 0; i < array.Length && i < array1.Length; i++)
+                Console.WriteLine("1./show-recent-currency-rates");
+                Console.WriteLine("2./find-currency-rate-by-code");
+                Console.WriteLine("3./calculate-amount-by-currecy");
+                Console.WriteLine("4./exit");
+
+                string[] array = { "USD", "RUB", "TRY" };
+                double[] array1 = { 1.70, 0.0271, 0.1031 };
+
+                double amount = Convert.ToDouble(Console.ReadLine());
+                bool notFound = true;     //teacher, burada bir neferden komek almisam. 
+
+                if (amount == 1)
                 {
-                    Console.Write($"{array[i]}:");
-                    Console.WriteLine(array1[i]);
-                }
-
-
-            }
-
-            else if (amount == 2)
-            {
-                Console.WriteLine("Which currency do you want to see");
-                string currency = Console.ReadLine();
-
-
-
-                for (int i = 0; i < array1.Length; i++)
-                {
-
-                    if (currency == array[i])
+                    for (int i = 0; i < array.Length && i < array1.Length; i++)
                     {
-                        Console.WriteLine(array1[i]);
-                        notFound = false;
+                        Console.Write($"{array[i]}: ");
+                        Console.WriteLine($"{array1[i]} AZN");
                     }
                 }
 
-                if (notFound)
+                else if (amount == 2)
                 {
-                    Console.WriteLine("Tapilmadi");
-                }
+                    Console.WriteLine("Which currency do you want to see?");
+                    string currency = Console.ReadLine();
 
-            }
-
-            else if (amount == 3)
-            {
-                Console.WriteLine("Meblegi daxil edin");
-                double mebleg = Convert.ToDouble(Console.ReadLine());
-                Console.WriteLine("Valyutbi secin");
-                string mvalyuta = Console.ReadLine();
-                
-                for (int i = 0; i < array1.Length; i++)
-                {
-
-                    if (mvalyuta == array[i])
+                    for (int i = 0; i < array1.Length; i++)
                     {
-                        //double 
-
-
-                        double netice = mebleg / array1[i];
-                        Console.Write(netice);
-                        
-                        Console.WriteLine("azn");
-                       
-                        
-                        notFound = false;
+                        if (currency == array[i])
+                        {
+                            Console.WriteLine($"{array1[i]} AZN");
+                            notFound = false;
+                        }
                     }
-
+                    if (notFound)
+                    {
+                        Console.WriteLine("Not found, please, try another currency.");
+                    }
                 }
-                if (notFound)
+                else if (amount == 3)
                 {
-                    Console.WriteLine("Tapilmadiiiii");
-                }
+                    Console.WriteLine("Enter the amount.");
+                    double sum = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Choose the currency what you want convert.");
+                    string currency = Console.ReadLine();
 
+                    for (int i = 0; i < array1.Length; i++)
+                    {
+                        if (currency == array[i])
+                        {
+                            double netice = sum / array1[i];
+                            Console.Write($"Result: {netice}");
+                            Console.WriteLine($" {array[i]}");
+                            notFound = false;
+                        }
+                    }
+                    if (notFound)
+                    {
+                        Console.WriteLine("Not found, please write the currency correctly.");
+                    }
+                }
+                else if (amount == 4)
+                {
+                    Console.WriteLine("Program finished.");
+                }
+                Console.ReadLine();
             }
-            else if (amount == 4)
-            {
-                Console.WriteLine("Stop");
-            }
-            Console.ReadLine();
         }
     }
 }
